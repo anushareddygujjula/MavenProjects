893ecf13b7110231180f4d63331a393425469b2a
anushagujjula <gujjulaanusha0@gmail.com>
2024-02-08 14:37:43 -0600

    added mutlithread and test cases

---
+++ src/test/java/OddEvenPrinterTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import multithreading.OddEvenPrinter;
+public class OddEvenPrinterTest {
+
+    @Test
+    public void testOddEvenPrinting() throws InterruptedException {
+        System.out.println("Test Case:");
+        OddEvenPrinter printer = new OddEvenPrinter();
+        printer.printOddEvenNumbers();
+        // To ensure that test completes after threads execution
+        Thread.sleep(1000); // Wait for 1 second
+    }
+}
